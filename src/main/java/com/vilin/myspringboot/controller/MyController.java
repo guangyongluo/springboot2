c5f2eecd3df5fa9d22a00450dae227fc3ab4e3c8
guangyongluo <llooww@mail.ustc.edu.cn>
2019-07-01 15:00:49 +0800

    springboot2(/static/css/*;/static/fonts/*;/static/images/*;/static/js/*;/templates/idx.html;com.vilin.myspringboot.controller.MyController.java) : add some static resources for springboot web demo.

--- src/main/java/com/vilin/myspringboot/controller/MyController.java
+++ src/main/java/com/vilin/myspringboot/controller/MyController.java
@@ -4,11 +4,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Date;
+
 @Controller
 public class MyController {
+
     @RequestMapping("/out")
     @ResponseBody
     public String out(){
         return "success";
+    }
+
+    @RequestMapping("/")
+    public String index(){
+        //访问idx.html
+        return "idx";
+    }
+
+    @RequestMapping("/date")
+    @ResponseBody //可以帮我们将返回的对象JSON序列化
+    public Date date(Date date){
+        return date;
     }
 }
