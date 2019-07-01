e62041bb7e537a539c003e7fd34d32786209139e
guangyongluo <llooww@mail.ustc.edu.cn>
2019-07-01 17:30:50 +0800

    springboot2(/config.properties;com.vilin.myspringboot.MyController.java;com.vilin.myspringboot.MySpringBootApplication.java) : personality springboot configuration.

--- src/main/java/com/vilin/myspringboot/MySpringBootApplication.java
+++ src/main/java/com/vilin/myspringboot/MySpringBootApplication.java
@@ -2,10 +2,13 @@ package com.vilin.myspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 //我是一个入口类，运行我就可以启动springboot应用，运行之后会自动扫描当前包和子包下
 //可以被注入的类包括：@Repository, @Service, @Controller, @Component, ,@Entity
 @SpringBootApplication
+//在入口类启动时加载config.properties
+@PropertySource("classpath:config.properties")
 public class MySpringBootApplication {
     public static void main(String[] args) {
         SpringApplication.run(MySpringBootApplication.class);
--- src/main/java/com/vilin/myspringboot/controller/MyController.java
+++ src/main/java/com/vilin/myspringboot/controller/MyController.java
@@ -1,13 +1,26 @@
 package com.vilin.myspringboot.controller;
 
+import com.vilin.myspringboot.config.AppConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
 import java.util.Date;
 
 @Controller
 public class MyController {
+
+    @Value("${app.name}")
+    private String name;
+
+    @Value("${app.page-size}")
+    private Integer pageSize;
+
+    //动态注入IOC容器中匹配的Bean
+    @Resource    //相同功能可使用@Autowire
+    private AppConfig appConfig;
 
     @RequestMapping("/out")
     @ResponseBody
