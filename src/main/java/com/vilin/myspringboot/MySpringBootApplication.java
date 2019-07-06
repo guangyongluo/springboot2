c5c1ff1b79ff9cc57ed5dae268c173d766c3345a
guangyongluo <llooww@mail.ustc.edu.cn>
2019-07-06 20:26:00 +0800

    springboot2(com.vilin.myspringboot.MySpringBootApplication.java): SpringBoot Druid integration.

--- src/main/java/com/vilin/myspringboot/MySpringBootApplication.java
+++ src/main/java/com/vilin/myspringboot/MySpringBootApplication.java
@@ -1,11 +1,22 @@
 package com.vilin.myspringboot;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import com.sun.org.apache.regexp.internal.RE;
 import com.vilin.myspringboot.common.filter.AccessRecorderFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.RegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 //我是一个入口类，运行我就可以启动springboot应用，运行之后会自动扫描当前包和子包下
 //可以被注入的类包括：@Repository, @Service, @Controller, @Component, ,@Entity
@@ -13,6 +24,40 @@ import org.springframework.context.annotation.PropertySource;
 //在入口类启动时加载config.properties
 @PropertySource("classpath:config.properties")
 public class MySpringBootApplication {
+
+    //手动初始化DruidDataSource对象
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource druidSource(){
+        DruidDataSource ds = new DruidDataSource();
+        return ds;
+    }
+
+    //注册后台的Servlet Bean，用于显示后台界面
+    @Bean
+    public ServletRegistrationBean statViewServlet(){
+        //创建StatViewServlet绑定到/druid路径下
+        //运行后可以通过/druid访问druid控制台
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+        Map<String, String> param = new HashMap<String, String>();
+        param.put("loginUsername","admin");
+        param.put("loginPassword","123456");
+        param.put("allow","");
+        servletRegistrationBean.setInitParameters(param);
+        return servletRegistrationBean;
+    }
+
+    //用于监听应用数据，filter用于收集数据，servlet用于展现数据
+    @Bean
+    public FilterRegistrationBean webStatFilter(){
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        Map<String, String> param = new HashMap<String, String>();
+        param.put("exclusions", "*.js,*.css");
+        filterRegistrationBean.setInitParameters(param);
+        return filterRegistrationBean;
+    }
 
     //注册Filter
     //@Bean会将方法中的返回对象在SpringBoot启动的时候放入IOC容器
